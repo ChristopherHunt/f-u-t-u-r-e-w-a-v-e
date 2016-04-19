3a38f899733b64952770a18309e3b05e727e6dea
James Bilous <JBillz@pcp143145pcs.wireless.calpoly.edu>
2016-04-19 14:45:17 -0700

    fixing some gitignore issues

--- src/app/midi_file_app/midi_file_app.h
+++ src/app/midi_file_app/midi_file_app.h
@@ -1,6 +1,6 @@
 #include "midifile/include/MidiFile.h"
 #include "midifile/include/Options.h"
-#include "/portmidi/include/portmidi.h"
-#include "/portmidi/include/porttime.h"
+#include "portmidi/include/portmidi.h"
+#include "portmidi/include/porttime.h"
 
 void printMidiPacketInfo(MidiEvent event);
