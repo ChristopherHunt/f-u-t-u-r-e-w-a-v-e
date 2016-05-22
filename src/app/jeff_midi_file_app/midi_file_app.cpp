dc4051f6a6300c03b346ebb2b13f75c535f580f0
jmcgover <jmcgover@calpoly.edu>
2016-05-22 13:40:31 -0700

    added some debug stuffs

--- src/app/jeff_midi_file_app/midi_file_app.cpp
+++ src/app/jeff_midi_file_app/midi_file_app.cpp
@@ -16,6 +16,7 @@ int main(int argc, char **argv) {
     }
 
     MidiFile midifile;
+    DEBUG_MSG("READING FILE");
     midifile.read(options.getArg(1));
     if(!midifile.status())
     {
@@ -27,13 +28,15 @@ int main(int argc, char **argv) {
     cout << "Ticks per quarter note: " << midifile.getTicksPerQuarterNote() << endl;
     cout << "Is absolute time? : " << midifile.isAbsoluteTicks() << endl;
     cout << "Is delta time? : " << midifile.isDeltaTicks() << endl;
-    int track = 6;
-    
+    int track = 5;
+    DEBUG_MSG("DISPLAYING MIDI FILE");
+    fprintf(stderr, "track: %d\n", track);
+    fprintf(stderr, "midifile.size(): %d\n", midifile.size());
     for(int i=0; i < midifile[track].size(); i++)
     {
         MidiEvent event = (MidiEvent)midifile[track][i];
         cout << "Midi event at tick " << event.tick << endl;
-        printMidiPacketInfo(event);         
+        printMidiPacketInfo(event);
     }
 
     // Test out port midi
@@ -46,16 +49,19 @@ int main(int argc, char **argv) {
     cout << "opening device for writing..." << endl;
     int32_t bufferSize = 1024;
 
+    DEBUG_MSG("NEW PortMidiStream");
     PortMidiStream** stream = new PortMidiStream*;
     PtError timeError = Pt_Start(1, NULL, NULL); 
     if (timeError != 0)
     {
         cout << "Error starting timer: " << timeError;
         exit(1);
-    }    
+    }
     cout << "Timer started" << endl;
 
-    PmError midiError = Pm_OpenOutput(stream, defaultDeviceID, NULL, 256, (PmTimestamp (*)(void *))&Pt_Time, NULL, 100); 
+    DEBUG_MSG("Pm_OpenOutput");
+    PmError midiError = Pm_OpenOutput(stream, defaultDeviceID, NULL, 256,
+                          (PmTimestamp (*)(void *))&Pt_Time, NULL, 100);
 
     if (midiError != 0)
     {
@@ -65,14 +71,14 @@ int main(int argc, char **argv) {
 
     cerr << "Opened midi device successfully!" << endl;
 
-int time = 0;
-    //for(int i=0; i < midifile[1].size(); i++)
+    int time = 0;
     for(int i=0; i < midifile[1].size(); i++)
     {
         MidiEvent event = (MidiEvent)midifile[track][i];
         //Convert a MidiEvent into a Portmidi message
         if (event.isNoteOn() || event.isNoteOff())
         {
+            DEBUG_MSG("NOTE ON/OFF EVENT");
             //Copy midi data into a PmEvent buffer
             PmMessage message = Pm_Message(event[0], event[1], event[2]);
             cout << dec << event.tick;
@@ -86,17 +92,20 @@ int time = 0;
             cout << "Playing at time: " << midifile.getTimeInSeconds(event.tick) * 1000.0 << endl;
             pmEvent->timestamp = 1000.0 * midifile.getTimeInSeconds(event.tick);
             pmEvent->message = message;
-            
+
             PmError sendError = Pm_Write(*stream, pmEvent, 24);
         }
         else
         {
+            DEBUG_MSG("OTHER EVENT");
             PmEvent *pmEvent = new PmEvent;
-            pmEvent->message = Pm_Message(event[0], event[1], event[2]); 
-            Pm_Write(*stream, pmEvent, 1); 
+            pmEvent->message = Pm_Message(event[0], event[1], event[2]);
+            DEBUG_MSG("OTHER EVENT:WRITING");
+            Pm_Write(*stream, pmEvent, 1);
         }
     }
-    
+
+    DEBUG_MSG("CLOSING STREAM");
     Pm_Close(stream);
     Pm_Terminate();
 }
@@ -110,7 +119,7 @@ void printMidiPacketInfo(MidiEvent event)
         if(event.isTempo())
         {
             cout << "tempo: " << event.getTempoBPM() << "bpm" << endl;
-        } 
+        }
     }
 
     if(event.isController())
@@ -126,12 +135,12 @@ void printMidiPacketInfo(MidiEvent event)
     if(event.isPressure())
     {
         cout << "\t Is Pressure message: " <<  event[1] << endl;
-        
+
     }
 
     if(event.isNoteOn())
     {
-        cout << "\t Is note on: " << event.getKeyNumber() << endl; 
+        cout << "\t Is note on: " << event.getKeyNumber() << endl;
     }
 
     if(event.isNoteOff())
