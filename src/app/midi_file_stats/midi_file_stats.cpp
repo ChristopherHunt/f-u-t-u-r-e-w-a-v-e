8ef5c0d3237ae27e0db662c409c9fa3292bfa459
jmcgover <jmcgover@calpoly.edu>
2016-05-22 16:29:41 -0700

    printing more information. still can't get durations to make sense.

--- src/app/midi_file_stats/midi_file_stats.cpp
+++ src/app/midi_file_stats/midi_file_stats.cpp
@@ -13,7 +13,9 @@
 #include <iostream>
 
 #include <errno.h>
+#include <float.h>
 #include <getopt.h>
+#include <limits.h>
 #include <stdio.h>
 #include <unistd.h>
 
@@ -52,15 +54,17 @@ int parse_args(Args *args, int argc, char **argv) {
     return EXIT_SUCCESS;
 }
 
-int write_midi_file_stats(ostream& out, MidiFile midifile) {
+int write_file_stats(ostream& out, MidiFile midifile) {
     WRITELN(out);
     WRITE_DESC(out, "MIDI Filename         : ", midifile.getFilename());
     WRITE_DESC(out, "Ticks Per Quarter Note: ", midifile.getTicksPerQuarterNote());
     WRITE_DESC(out, "Is Absolute Time?     : ", midifile.isAbsoluteTicks());
     WRITE_DESC(out, "Is Delta Time?        : ", midifile.isDeltaTicks());
-    WRITE_DESC(out, "Number of Tracks      : ", midifile.getTrackCount());
     WRITE_DESC(out, "Has Joined Tracks?    : ", midifile.hasJoinedTracks());
     WRITE_DESC(out, "Has Split Tracks?     : ", midifile.hasSplitTracks());
+    WRITELN(out);
+    WRITE_DESC(out, "Number of Tracks      : ", midifile.getTrackCount());
+    WRITE_DESC(out, "Size (Num Tracks?)    : ", midifile.size());
     WRITELN(out);
     WRITE_STR(out, "Performing Time Analysis...");
     midifile.doTimeAnalysis();
@@ -68,16 +72,65 @@ int write_midi_file_stats(ostream& out, MidiFile midifile) {
     return EXIT_SUCCESS;
 }
 
-int write_midi_event_stats(ostream& out, MidiFile midifile) {
+int write_track_stats(ostream& out, MidiFile midifile) {
+    for (int i = 0; i < midifile.getTrackCount(); i++) {
+        cout << "Track " << i << " Size: " << midifile[i].getSize() << endl;
+    }
     return EXIT_SUCCESS;
 }
 
-int print_file_stats(MidiFile midifile) {
-    return write_midi_file_stats(cout, midifile);
+int write_event_information(ostream& out, MidiEvent event) {
+    return EXIT_SUCCESS;
 }
 
-int print_event_stats(MidiFile midifile) {
-    return write_midi_event_stats(cout, midifile);
+int write_event_stats(ostream& out, MidiFile midifile) {
+    MidiEvent event;
+    for (int i = 0; i < midifile.getTrackCount(); i++) {
+        cout << "Track " << i << " Size: " << midifile[i].getSize() << endl;
+        int event_total_size = 0;
+        int event_total_duration = 0;
+        int event_num = 0;
+        int size_max = INT_MIN;
+        int size_min = INT_MAX;
+        MidiEvent event_size_min;
+        MidiEvent event_size_max;
+        double duration_max = DBL_MIN;
+        double duration_min = DBL_MAX;
+        MidiEvent event_duration_min;
+        MidiEvent event_duration_max;
+        for (int j = 0; j < midifile[i].getSize(); j++) {
+            midifile[i].linkNotePairs();
+            int this_size = midifile[i][j].getSize();
+            int this_duration = midifile[i][j].getDurationInSeconds();
+            //char this_command = midifile[i][j].getCommand();
+            if  (this_size > size_max) {
+                size_max = this_size;
+                event_size_max = midifile[i][j];
+            }
+            if  (this_size < size_min) {
+                size_min = this_size;
+                event_size_min = midifile[i][j];
+            }
+            if  (this_duration > duration_max) {
+                duration_max = this_duration;
+                event_duration_max = midifile[i][j];
+            }
+            if  (this_duration < duration_min) {
+                duration_min = this_duration;
+                event_duration_min = midifile[i][j];
+            }
+            event_total_size += this_size;
+            event_total_duration += this_duration;
+            event_num++;
+        }
+        cout << "\tMinimum Event Size: " << size_min << endl;
+        cout << "\tMaximum Event Size: " << size_max << endl;
+        cout << "\tAverage Event Size: " << (double)event_total_size / event_num << endl;
+        cout << "\tMinimum Event Duration: " << duration_min << endl;
+        cout << "\tMaximum Event Duration: " << duration_max << endl;
+        cout << "\tAverage Event Duration: " << (double)event_total_duration / event_num << endl;
+    }
+    return EXIT_SUCCESS;
 }
 
 int main(int argc, char **argv) {
@@ -100,13 +153,19 @@ int main(int argc, char **argv) {
     /*
      * Print MIDI File Stats
      */
-    PRINT_STR("Printing File Stats...");
-    print_file_stats(midifile);
+    PRINT_STR("Printing FILE Stats...");
+    write_file_stats(cout, midifile);
+
+    /*
+     * Print MIDI Track Stats
+     */
+    PRINT_STR("Printing TRACK Stats...");
+    write_track_stats(cout, midifile);
 
     /*
      * Print MIDI Event Stats
      */
-    PRINT_STR("Printing Event Stats...");
-    print_event_stats(midifile);
+    PRINT_STR("Printing EVENT Stats...");
+    write_event_stats(cout, midifile);
     return 0;
 }
