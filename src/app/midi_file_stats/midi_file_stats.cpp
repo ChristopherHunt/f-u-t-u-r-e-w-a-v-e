7b6fc9ef8a107ecc7f45b9415f6db3b10c7f594c
jmcgover <jmcgover@calpoly.edu>
2016-05-22 14:59:01 -0700

    Added macros and more print statements in the stats.

--- src/app/midi_file_stats/midi_file_stats.cpp
+++ src/app/midi_file_stats/midi_file_stats.cpp
@@ -1,7 +1,7 @@
 /*
  * MIDI File Stats Printer
  *
- * Prints the statistics for a MIDI file, including 
+ * Prints the statistics for a MIDI file, including
  * -- Number of each MIDI command used
  * -- Min time between MIDI commands
  * -- Max time between MIDI commands
@@ -10,10 +10,20 @@
 
 #include "midi_file_stats.h"
 
+#include <iostream>
+
 #include <errno.h>
 #include <getopt.h>
 #include <stdio.h>
 #include <unistd.h>
+
+#define WRITE_STR(outstream, str)\
+    ((outstream) << (str) << endl)
+#define PRINT_STR(str) WRITE_STR(cout, str)
+#define WRITE_DESC(outstream, description, value)\
+    ((outstream) << (description) << (value) << endl)
+#define WRITELN(outstream) outstream << endl
+#define PRINTLN() WRITELN(cout)
 
 using namespace std;
 
@@ -42,11 +52,61 @@ int parse_args(Args *args, int argc, char **argv) {
     return EXIT_SUCCESS;
 }
 
+int write_midi_file_stats(ostream& out, MidiFile midifile) {
+    WRITELN(out);
+    WRITE_DESC(out, "MIDI Filename         : ", midifile.getFilename());
+    WRITE_DESC(out, "Ticks Per Quarter Note: ", midifile.getTicksPerQuarterNote());
+    WRITE_DESC(out, "Is Absolute Time?     : ", midifile.isAbsoluteTicks());
+    WRITE_DESC(out, "Is Delta Time?        : ", midifile.isDeltaTicks());
+    WRITE_DESC(out, "Number of Tracks      : ", midifile.getTrackCount());
+    WRITE_DESC(out, "Has Joined Tracks?    : ", midifile.hasJoinedTracks());
+    WRITE_DESC(out, "Has Split Tracks?     : ", midifile.hasSplitTracks());
+    WRITELN(out);
+    WRITE_STR(out, "Performing Time Analysis...");
+    midifile.doTimeAnalysis();
+    WRITE_STR(out, "Done with Time Analysis.");
+    return EXIT_SUCCESS;
+}
+
+int write_midi_event_stats(ostream& out, MidiFile midifile) {
+    return EXIT_SUCCESS;
+}
+
+int print_file_stats(MidiFile midifile) {
+    return write_midi_file_stats(cout, midifile);
+}
+
+int print_event_stats(MidiFile midifile) {
+    return write_midi_event_stats(cout, midifile);
+}
+
 int main(int argc, char **argv) {
     int rtn = 0;
     Args args = {NULL};
+
+    /*
+     * Parse Arguments
+     */
     rtn = parse_args(&args, argc, argv);
     ASSERT_MSG(!rtn, "Arguments failed to parse");
-    printf("Opening file: %s\n", args.filename);
+    /*
+     * Open File
+     */
+    MidiFile midifile;
+    PRINTLN();
+    printf("Opening file '%s'...\n", args.filename);
+    midifile.read(args.filename);
+    ASSERT_MSG(!errno, "Error reading MIDI File");
+    /*
+     * Print MIDI File Stats
+     */
+    PRINT_STR("Printing File Stats...");
+    print_file_stats(midifile);
+
+    /*
+     * Print MIDI Event Stats
+     */
+    PRINT_STR("Printing Event Stats...");
+    print_event_stats(midifile);
     return 0;
 }
